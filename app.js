bd3d05618d7a2856506db9d3aba975f0aa99e269
Richard Lara <richardlara67@gmail.com>
2021-05-10 17:56:39 -0500

    model basics

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+//required libraries
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campground')
+
+//connect to mongo
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+//verify connection to database
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -13,6 +31,11 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My backyard', description: 'cheap camping' });
+    await camp.save();
+    res.send(camp)
+})
 /***** End Rountings ********/
 
 app.listen(3000, () => {
