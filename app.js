4481ac77e18b784a96f2f4647c0c6e87772e8684
Richard Lara <richardlara67@gmail.com>
2021-05-11 00:58:40 -0500

    Added "show" view

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+//look up campground by id
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
 /***** End Rountings ********/
 
 app.listen(3000, () => {
