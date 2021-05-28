febbbe3a24f41138e4e3d0731004250b96f00626
Richard Lara <richardlara67@gmail.com>
2021-05-28 14:49:51 -0500

    Added Ejs-Mate as ejs engine and configured the basic HTML layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { findByIdAndDelete } = require('./models/campground');
@@ -22,6 +23,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -38,7 +40,7 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', async(req, res) => {
+app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 })
@@ -48,32 +50,32 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async(req, res) => {
+app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
 })
 
 //look up campground by id
-app.get('/campgrounds/:id', async(req, res) => {
+app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 })
 
 //edit campground
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground })
 })
 
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
 //delete campground
-app.delete('/campgrounds/:id', async(req, res) => {
+app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
