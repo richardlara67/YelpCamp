1f314d0e1377448a5b0b46b3e083391f60e6ff94
Richard Lara <richardlara67@gmail.com>
2021-05-29 12:32:45 -0500

    Styled the New Campground page

--- seeds/index.js
+++ seeds/index.js
@@ -20,14 +20,18 @@ db.once('open', () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-const seedDb = async() => {
+const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             line: i + 1,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis optio impedit sint pariatur non quos dolore ut similique, reprehenderit beatae modi labore voluptatum natus, voluptas soluta accusamus fugiat officiis! Aspernatur!',
+            price
         })
         await camp.save();
     }
